40ed3b0b96bc3bf9145aac41e1c0a1c6789c2a6a
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-10-24 14:44:03 -0300

    fix: login and signup pages style

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -8,9 +8,11 @@ import { ThreeDots } from "react-loader-spinner";
 
 const Container = styled.div`
     width: 100%;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     img {
         width: 180px;
         height: 180px;
